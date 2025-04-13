52154490325aebbbbf4359b794ec1b77f5ddaf18
Priyanshi Kirar <patelpriyanshi866@gmail.com>
2025-04-13 22:48:13 +0530

    13 april

--- server/src/middlewares/errorMiddleware.js
+++ server/src/middlewares/errorMiddleware.js
@@ -1,12 +1,11 @@
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-  
-    res.status(statusCode).json({
-      // success: false,
-      message: err.message || "Internal Server Error",
-    });
-  };
-  
-  // ✅ Correctly export the function as default
-  export default errorHandler;
-  
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+
+  res.status(statusCode).json({
+    // success: false,
+    message: err.message || "Internal Server Error",
+  });
+};
+
+// ✅ Correctly export the function as default
+export default errorHandler;
